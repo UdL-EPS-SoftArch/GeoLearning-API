0f0712278a7160c943612970fb7abebb2b2a4e6f
Roberto García <roberto@rhizomik.net>
2020-02-29 19:58:16 +0100

    Create sample demo ContentCreator for logged in registration scenario

--- src/main/java/cat/udl/eps/softarch/geolearning/config/AuthenticationConfig.java
+++ src/main/java/cat/udl/eps/softarch/geolearning/config/AuthenticationConfig.java
@@ -1,11 +1,9 @@
 package cat.udl.eps.softarch.geolearning.config;
 
-//import cat.udl.eps.softarch.geolearning.domain.Player;
-
 import cat.udl.eps.softarch.geolearning.domain.ContentCreator;
 import cat.udl.eps.softarch.geolearning.domain.User;
-//import cat.udl.eps.softarch.geolearning.repository.PlayerRepository;
 import cat.udl.eps.softarch.geolearning.repository.ContentCreatorRepository;
+import cat.udl.eps.softarch.geolearning.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -18,17 +16,9 @@ public class AuthenticationConfig extends GlobalAuthenticationConfigurerAdapter 
     @Value("${default-password}")
     String defaultPassword;
 
-    @Autowired
-    BasicUserDetailsService basicUserDetailsService;
-
-  /*
-  @Autowired
-  PlayerRepository playerRepository;
-  */
-
-    @Autowired
-    ContentCreatorRepository contentCreatorRepository;
-
+    @Autowired BasicUserDetailsService basicUserDetailsService;
+    @Autowired UserRepository userRepository;
+    @Autowired ContentCreatorRepository contentCreatorRepository;
 
     @Override
     public void init(AuthenticationManagerBuilder auth) throws Exception {
@@ -36,37 +26,14 @@ public class AuthenticationConfig extends GlobalAuthenticationConfigurerAdapter 
                 .userDetailsService(basicUserDetailsService)
                 .passwordEncoder(User.passwordEncoder);
 
-    /*
-        // Sample player
-    if (!playerRepository.existsById("demoP")) {
-      Player player = new Player();
-      player.setEmail("demoP@geoLearning.game");
-      player.setUsername("demoP");
-      player.setPassword(defaultPassword);
-      player.encodePassword();
-      playerRepository.save(player);
-    }
-
-    // Sample player
-    if (!playerRepository.existsById("demoP1")) {
-      Player player = new Player();
-      player.setEmail("demoP1@geoLearning.game");
-      player.setUsername("demoP1");
-      player.setPassword(defaultPassword);
-      player.encodePassword();
-      playerRepository.save(player);
-    }
-
-     */
-
-        // Sample Tournament Master
-        if (!contentCreatorRepository.existsById("demoCC")) {
-            ContentCreator admin = new ContentCreator();
-            admin.setEmail("demoCC@geoLearning.game");
-            admin.setUsername("demoCC");
-            admin.setPassword(defaultPassword);
-            admin.encodePassword();
-            contentCreatorRepository.save(admin);
+        // Sample Content Creator
+        if (!userRepository.existsById("demo")) {
+            User user = new ContentCreator();
+            user.setEmail("demo@sample.app");
+            user.setUsername("demo");
+            user.setPassword(defaultPassword);
+            user.encodePassword();
+            userRepository.save(user);
         }
     }
 }
