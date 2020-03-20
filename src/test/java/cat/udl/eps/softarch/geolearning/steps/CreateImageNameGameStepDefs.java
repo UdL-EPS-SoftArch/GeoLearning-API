63fba9c6a76bc2e3b9b8e318634722763eb06fc2
aidagorguesmanzanares <aida.gorgues@gmail.com>
2020-03-20 19:06:59 +0100

    Create java CreateImageNameGameStepDefs
    Signed-off-by: aidagorguesmanzanares <aida.gorgues@gmail.com>

---
+++ src/test/java/cat/udl/eps/softarch/geolearning/steps/CreateImageNameGameStepDefs.java
@@ -0,0 +1,140 @@
+package cat.udl.eps.softarch.geolearning.steps;
+
+import cat.udl.eps.softarch.geolearning.domain.ImageNameWriteQuestion;
+import cat.udl.eps.softarch.geolearning.repository.ImageNameWriteQuestionRepository;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import cat.udl.eps.softarch.geolearning.domain.Game;
+import cat.udl.eps.softarch.geolearning.domain.ImageName;
+import cat.udl.eps.softarch.geolearning.domain.User;
+import cat.udl.eps.softarch.geolearning.repository.GameRepository;
+import cat.udl.eps.softarch.geolearning.repository.UserRepository;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.And;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+public class CreateImageNameGameStepDefs {
+
+    @Autowired
+    private StepDefs stepDefs;
+
+    @Autowired
+    private GameRepository gameRepository;
+    ImageNameWriteQuestionRepository ImageNameWriteQuestionRepository;
+    ImageName imageName;
+    private String newResourceUri;
+    ImageNameWriteQuestion imageNameWriteQuestion1;
+    ImageNameWriteQuestion imageNameWriteQuestion2;
+
+
+
+
+    @When("^I create a new ImageName with instructions \"([^\"]*)\"")
+    public void iCreateANewImageNameWithInstructions(String instructions) throws Throwable {
+        imageName = new ImageName();
+        imageName.setInstructions(instructions);
+        stepDefs.result = stepDefs.mockMvc.perform(
+                post("/imageNames")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                stepDefs.mapper.writeValueAsString(imageName))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+        newResourceUri = stepDefs.result.andReturn().getResponse().getHeader("Location");
+    }
+
+    @And("^It has been created a ImageName with instructions \\\"([^\\\"]*)\\\"")
+    public void itHasBeenCreatedAImageNameWithInstructions(String instructions) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get(newResourceUri)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print())
+                .andExpect(jsonPath("$.instructions", is(instructions)));
+    }
+
+
+    @And("^It has not been created an ImageName")
+    public void itHasNotBeenCreatedAnImageName() throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get(newResourceUri)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print())
+                .andExpect(status().isNotFound());      //Això pot ser que no funcioni
+    }
+
+    @When("^I create a new ImageName without instructions")
+    public void iCreateANewImageNameWithoutInstructions() throws Throwable {
+        imageName = new ImageName();
+        stepDefs.result = stepDefs.mockMvc.perform(
+                post("/imageNames")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                stepDefs.mapper.writeValueAsString(imageName))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+        newResourceUri = stepDefs.result.andReturn().getResponse().getHeader("Location");
+    }
+
+    @When("I create a new ImageName with instructions \\\"([^\\\"]*)\\\" and question with \\\"([^\\\"]*)\\\" and \\\"([^\\\"]*)\\\" and question with \\\"([^\\\"]*)\\\" and \\\"([^\\\"]*)\\\"")
+    public void iCreateANewImageWithInstructionsAndQuestions(String instructions, String image1, String response1, String image2, String response2) throws Throwable {
+        imageName = new ImageName();
+        imageName.setInstructions(instructions);
+        imageNameWriteQuestion1 = new ImageNameWriteQuestion();
+        imageNameWriteQuestion2 = new ImageNameWriteQuestion();
+
+        imageNameWriteQuestion1.setImage(image1);
+        imageNameWriteQuestion1.setSolution(response1);
+        imageNameWriteQuestion2.setImage(image2);
+        imageNameWriteQuestion2.setSolution(response2);
+
+        //No sabem si hem de fer el set, ja que no sabem si relacionara la llista amb el que estem guardant a la base de dades
+        //ImageNameWriteQuestionRepository.save(imageNameWriteQuestion1);
+        //ImageNameWriteQuestionRepository.save(imageNameWriteQuestion2);
+        
+        List<ImageNameWriteQuestion> questions = new ArrayList<>();
+        questions.add(imageNameWriteQuestion1);
+        questions.add(imageNameWriteQuestion2);
+        
+        imageName.setQuestions(questions);
+
+        stepDefs.result = stepDefs.mockMvc.perform(
+                post("/imageNames")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                stepDefs.mapper.writeValueAsString(imageName))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+        newResourceUri = stepDefs.result.andReturn().getResponse().getHeader("Location");
+    }
+
+    @And("^It has been created a ImageName with instructions \\\"([^\\\"]*)\\\" and question with \\\"([^\\\"]*)\\\" and \\\"([^\\\"]*)\\\" and question with \\\"([^\\\"]*)\\\" and \\\"([^\\\"]*)\\\"")
+    public void itHasBeenCreatedAImageNameWithInstructionsAndQuestions(String instructions, String image1, String response1, String image2, String response2) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get(newResourceUri)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print())
+                .andExpect(jsonPath("$.instructions", is(instructions)))
+                .andExpect(jsonPath("$.questions[0].image", is(image1)))
+                .andExpect(jsonPath("$.questions[0].solution", is(response1)))
+                .andExpect(jsonPath("$.questions[1].image", is(image2)))
+                .andExpect(jsonPath("$.questions[1].solution", is(response2)));
+    }
+}
--- src/test/resources/features/CreateImageNameGame.feature
+++ src/test/resources/features/CreateImageNameGame.feature
@@ -7,28 +7,28 @@ Feature: ImageName
     Given I'm not logged in
     When I create a new ImageName with instructions "Relacionar bandera con paises"
     Then The response code is 401
-    And It has not been created a ImageName with instructions "Relacionar bandera con paises"
+    And It has not been created an ImageName
 
   Scenario: Create with normal user
     Given I login as "demo" with password "password"
     When I create a new ImageName with instructions "Relacionar bandera con paises"
-    Then The response code is 403
-    And It has not been created a ImageName with instructions "Elige la respuesta correcta"
+    Then The response code is 401
+    And It has not been created an ImageName
 
   Scenario: Create with Content Creator and no instructions
     Given I login as "creator" with password "password"
     When I create a new ImageName without instructions
     Then The response code is 403
-    And It has not been created a ImageName
+    And It has not been created an ImageName
 
   Scenario: Create with Content Creator and instructions
     Given I login as "creator" with password "password"
     When I create a new ImageName with instructions "Relacionar bandera con paises"
-    Then The response code is 201
+    Then The response code is 200
     And It has been created a ImageName with instructions "Relacionar bandera con paises"
 
   Scenario: Create with Content Creator, instructions and questions list
     Given I login as "creator" with password "password"
     When I create a new ImageName with instructions "Relacionar bandera con paises" and question with "image/japan" and "japan" and question with "image/spain" and "spain"
-    Then The response code is 201
+    Then The response code is 200
     And It has been created a ImageName with instructions "Relacionar bandera con paises" and question with "image/japan" and "japan" and question with "image/spain" and "spain"
