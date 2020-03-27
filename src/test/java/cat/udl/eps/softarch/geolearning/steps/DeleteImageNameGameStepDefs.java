d2d3492fde2264988e7d11f06eb77af245b08ae9
ar98nau <anr3@alumnes.udl.cat>
2020-03-27 17:45:10 +0100

    DeleteImageNameGame.feature
    Signed-off-by: ar98nau <anr3@alumnes.udl.cat>

--- src/test/java/cat/udl/eps/softarch/geolearning/steps/DeleteImageNameGameStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/geolearning/steps/DeleteImageNameGameStepDefs.java
@@ -10,6 +10,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,50 +29,25 @@ public class DeleteImageNameGameStepDefs {
 
 
     @Autowired
-    private ImageNameRepository imageNameRepository;
-    private ImageNameWriteQuestionRepository imageNameWriteQuestionRepository;
-    private ImageNameWriteQuestion imageNameWriteQuestion1;
-    private ImageNameWriteQuestion imageNameWriteQuestion2;
+    private GetImageNameGameStepDefs getImageNameGameStepDefs;
+    private ResultActions result;
 
-
-    private ImageName imageName;
-    private int id;
-    private String newResourceUri;
     private String questionsUri;
 
     @And("^There is an ImageName game with instructions \\\"([^\\\"]*)\\\" and question with \\\"([^\\\"]*)\\\" and \\\"([^\\\"]*)\\\" and question with \\\"([^\\\"]*)\\\" and \\\"([^\\\"]*)\\\"")
     public void thereIsAnImageNameGameWithInstructionsAndQuestions (String instructions, String image1, String response1, String image2, String response2) throws Throwable {
-        imageName = new ImageName();
-        imageName.setInstructions(instructions);
+        getImageNameGameStepDefs.thereIsAnImageNameWithInstructionsAndQuestions(instructions, image1, response1, image2, response2);
 
-        imageNameWriteQuestion1 = new ImageNameWriteQuestion();
-        imageNameWriteQuestion1.setImage(image1);
-        imageNameWriteQuestion1.setSolution(response1);
-        imageNameWriteQuestion1.setImageName(imageName);
-
-        imageNameWriteQuestion2 = new ImageNameWriteQuestion();
-        imageNameWriteQuestion2.setImage(image2);
-        imageNameWriteQuestion2.setSolution(response2);
-        imageNameWriteQuestion2.setImageName(imageName);
-
-        List<ImageNameWriteQuestion> questions = new ArrayList<>();
-        questions.add(imageNameWriteQuestion1);
-        questions.add(imageNameWriteQuestion2);
-
-        imageName.setQuestions(questions);
-
-        newResourceUri = imageNameRepository.save(imageName).getUri();
-
-        stepDefs.result = stepDefs.mockMvc.perform(
-                get(newResourceUri)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .with(AuthenticationStepDefs.authenticate()))
-                .andDo(print())
-                .andExpect(status().isOk());
+        result = stepDefs.mockMvc.perform(
+            get(getImageNameGameStepDefs.newResourceUri)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .with(AuthenticationStepDefs.authenticate()))
+            .andDo(print())
+            .andExpect(status().isOk());
 
         JsonParser parser = new JsonParser();
         JsonObject jsonObject;
-        String response = stepDefs.result.andReturn().getResponse().getContentAsString();
+        String response = this.result.andReturn().getResponse().getContentAsString();
         jsonObject = (JsonObject) parser.parse(response);
         jsonObject = (JsonObject) jsonObject.get("_links");
         jsonObject = (JsonObject) jsonObject.get("questions");
@@ -82,27 +58,16 @@ public class DeleteImageNameGameStepDefs {
     @When("^I delete the previous ImageName")
     public void iDeleteThePreviousImageName() throws Throwable {
         stepDefs.result = stepDefs.mockMvc.perform(
-                delete(newResourceUri)
+                delete(getImageNameGameStepDefs.newResourceUri)
                         .accept(MediaType.APPLICATION_JSON)
                         .with(AuthenticationStepDefs.authenticate()))
                 .andDo(print());
     }
 
-    @And("^Previous ImageName is still in the database")
-    public void previousImageNameIsStillInTheDatabase() throws Throwable {
-        stepDefs.result = stepDefs.mockMvc.perform(
-                get(newResourceUri)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .with(AuthenticationStepDefs.authenticate()))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-
-    @And("^Previous ImageName has been deleted")
+    @And("^Previous ImageName and its questions have been deleted")
     public void previousImageNameHasBeenDeleted() throws Throwable {
         stepDefs.result = stepDefs.mockMvc.perform(
-                get(newResourceUri)
+                get(getImageNameGameStepDefs.newResourceUri)
                         .accept(MediaType.APPLICATION_JSON)
                         .with(AuthenticationStepDefs.authenticate()))
                 .andDo(print())
