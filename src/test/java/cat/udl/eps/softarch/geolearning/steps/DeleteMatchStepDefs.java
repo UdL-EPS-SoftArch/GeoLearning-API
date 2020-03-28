13a7af55a4536c94ed5d20f9eb97e698a4114fe5
jassnorris <jassinformatic@gmail.com>
2020-03-28 21:47:29 +0100

    Delete Match Feature && Delete Match StepsDefs

---
+++ src/test/java/cat/udl/eps/softarch/geolearning/steps/DeleteMatchStepDefs.java
@@ -0,0 +1,99 @@
+package cat.udl.eps.softarch.geolearning.steps;
+
+import cat.udl.eps.softarch.geolearning.domain.*;
+import cat.udl.eps.softarch.geolearning.repository.*;
+import io.cucumber.core.internal.gherkin.deps.com.google.gson.Gson;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.data.web.JsonPath;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.util.ClassUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.print.attribute.standard.Media;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class DeleteMatchStepDefs{
+    @Autowired
+    private StepDefs stepDefs;
+
+    @Autowired
+    private MatchRepository matchRepository;
+
+    @Autowired
+    private ContentCreatorRepository creatorRepository;
+
+    @Autowired
+    private ImageImageRepository imageRepository;
+
+    @Autowired
+    private ImageOptionRepository imageOptionRepository;
+
+    private String newResourceUri;
+    private Integer postedId;
+
+
+    @When("I create a new match with name {string} and description {string} with rating {int}")
+    public void checkRegisteredMatch(String name, String description, Integer rating) throws Exception{
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get(newResourceUri)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate())
+        ).andDo(print())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.description", is(description)))
+                .andExpect(jsonPath("$.rating", is(rating)))
+        //.andExpect(jsonPath("$.games", is(games)))
+        //.andExpect(jsonPath("$.contentCreator", is(creatorRepository.findByEmail("creator@sample.app"))))
+        ;
+    }
+
+    @When("I delete match with name {string} and description {string} with rating {int}")
+    public void iDeleteAMatchtWithNameDescriptionAndRate(String name, String description , Integer rating) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                delete("/match/{name}", name)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+    }
+
+    /*@And("^It has not been deleted a match with name \"([^\"]*)\"$")
+    public void itHasNotBeenDeletedAMatchWithId(String name) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get("/match/{name}", name)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andExpect(status().is2xxSuccessful());
+    }*/
+
+
+    @And("^It does not exist a match with name \"([^\"]*)\"$")
+    public void itDoesNotExistAMatchWithName(String name) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get("/match/{name}", name)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+    }
+}
+
+
+
---
+++ src/test/resources/features/DeleteMatch.feature
@@ -0,0 +1,24 @@
+Feature: Delete Matches
+  In order use the app
+  As a content creator
+  I want to delete a match
+
+  Scenario: Match creator deletes match
+    Given I login as "creator" with password "password"
+    When I create a new match with name "match0" and description "one game match" with rating 10
+    When I delete match with name "match0" and description "one game match" with rating 10
+    Then The response code is 204
+    And It does not exist a match with name "match0"
+
+  Scenario: Player tries to delete a match
+    Given I login as "creator" with password "password"
+    When I create a new match with name "match0" and description "one game match" with rating 10
+    And I login as "user" with password "password"
+    When I delete match with name "match0" and description "one game match" with rating 10
+    Then The response code is 403
+
+  Scenario: Remove a match that does not exist
+    Given I login as "creator" with password "password"
+    When I delete match with name "match0" and description "one game match" with rating 10
+    Then The response code is 404
+
