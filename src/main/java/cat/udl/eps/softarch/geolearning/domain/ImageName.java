da7b25dd8f757050dd251d8b32406ba8f69781cc
ar98nau <anr3@alumnes.udl.cat>
2020-05-19 14:11:56 +0200

    isWrite boolean for ImageName Game alternative

--- src/main/java/cat/udl/eps/softarch/geolearning/domain/ImageName.java
+++ src/main/java/cat/udl/eps/softarch/geolearning/domain/ImageName.java
@@ -2,10 +2,14 @@ package cat.udl.eps.softarch.geolearning.domain;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
@@ -15,4 +19,9 @@ public class ImageName extends Game {
 
     @OneToMany(mappedBy = "imageName" ,cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ImageNameQuestion> questions;
+
+    @Column
+    @NotNull
+    private Boolean isWrite = false;
+
 }
